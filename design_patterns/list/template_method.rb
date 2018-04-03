a1c0c0608210b39f216a1b202e26b7ed69693e97
Nazarii <ivankivnazo@gmail.com>
2018-04-03 22:37:56 +0300

    design patterns: template method

---
+++ design_patterns/list/template_method.rb
@@ -0,0 +1,44 @@
+class AbstractClass
+  def initialize
+    @data = 'some data'  
+  end
+  
+  def template_method
+    action_one
+    action_two
+    action_three
+    action_four
+  end
+  
+  def action_one
+    puts "required action #{@data}"
+  end
+  
+  def action_two
+  end
+  
+  def action_three
+  end
+
+  def action_four
+  end
+end
+
+class SubclassOne < AbstractClass  
+  def action_two
+    puts 'ovveridden method'
+  end
+
+  def action_three
+    puts 'ovveridden method'
+  end
+end
+
+
+class SubclassTwo < AbstractClass
+end
+
+object = SubclassOne.new
+object.template_method
+object = SubclassTwo.new
+object.template_method
